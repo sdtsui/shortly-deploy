4108ee7cfb7cd599b10bd3eae66aa85a4beb49c9
sdtsui <szehungdanieltsui@gmail.com>
2015-01-14 20:02:57 -0800

    Update finish up to Mongo

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.listen(port);
 
 console.log('Server now listening on port ' + port);
 
+// Mongo URI
+// mongodb://MongoLab-v:poUVZNK8geNFwNKaZy0TDonuls4GaDXtX.S3apXh.wo-@ds052837.mongolab.com:52837/MongoLab-v
